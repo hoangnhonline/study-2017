83a33f742bc48b7b7be5847236279eccf8386402
HoangNH <hoangnhonline@gmail.com>
2017-10-07 21:28:07 +0700

    .

--- public/admin/dist/js/pages/dashboard.js
+++ public/admin/dist/js/pages/dashboard.js
@@ -4,44 +4,46 @@
  * Description:
  *      This is a demo file used only for the main dashboard (index.html)
  **/
+var h = screen.height;
+var w = screen.width;
+var left = (screen.width/2)-((w-300)/2);
+var top = (screen.height/2)-((h-100)/2);
 
-$(function () {
+function singleUpload(obj) {
+    window.KCFinder = {};
+    window.KCFinder.callBack = function(url) {
+       $('#' + obj.data('set')).val(url);
+       $('#' + obj.data('image')).attr('src', $('#app_url').val() + url);
+        window.KCFinder = null;
+    };
+    window.open($('#url_open_kc_finder').val(), 'kcfinder_single','scrollbars=1,menubar=no,width='+ (w-300) +',height=' + (h-300) +',top=' + top+',left=' + left);
+}
+
+
+$(document).ready(function(){
 
   "use strict";
 
-  //Make the dashboard widgets sortable Using jquery UI
-  $(".connectedSortable").sortable({
-    placeholder: "sort-highlight",
-    connectWith: ".connectedSortable",
-    handle: ".box-header, .nav-tabs",
-    forcePlaceholderSize: true,
-    zIndex: 999999
-  });
-  $(".connectedSortable .box-header, .connectedSortable .nav-tabs-custom").css("cursor", "move");
-
-  //jQuery UI sortable for the todo list
-  $(".todo-list").sortable({
-    placeholder: "sort-highlight",
-    handle: ".handle",
-    forcePlaceholderSize: true,
-    zIndex: 999999
+  $('.btnUploadSingle').click(function(){        
+    singleUpload($(this));
   });
 
-
-  //SLIMSCROLL FOR CHAT WIDGET
-  $('#chat-box').slimScroll({
-    height: '250px'
-  });
-
-  /* Morris.js Charts */
-
-
-  //Fix for charts under tabs
-  $('.box ul.nav a').on('shown.bs.tab', function () {
-    //area.redraw();
-    //donut.redraw();
-    //line.redraw();
-  });
+  $('#dataForm #name').change(function(){
+       var name = $.trim( $(this).val() );         
+        $.ajax({
+          url: $('#route_get_slug').val(),
+          type: "POST",
+          async: false,      
+          data: {
+            str : name
+          },              
+          success: function (response) {
+            if( response.str ){                  
+              $('#dataForm #slug').val( response.str );
+            }                
+          }
+        });         
+    });
 
 });
 var toolbar = [
