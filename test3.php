b65b8721793b26e07b9a52745e15a583c47ecd0c
HoangNH <hoangnhonline@gmail.com>
2017-10-19 13:02:53 +0700

    .

--- test3.php
+++ test3.php
@@ -1,17 +1,13 @@
 <?php 
-function microtime_float()
-  {
-      list($usec, $sec) = explode(" ", microtime());
-      return ((float)$usec + (float)$sec);
-  }
-set_time_limit(10000);
-echo $start = microtime_float();
-echo "<hr>";
-for( $i = 1; $i <= 1000; $i ++ ){
-	$str = str_repeat($i.",", $i);
-	echo rtrim($str, ",")."<br>";
+function test($maxNumber = 5){
+	for ($i = 1; $i <= 5; $i ++) {
+		$tmpStr = "";
+		for ($j = 1; $j <= $i; $j ++) {
+			$tmpStr .= $i . ",";			
+		}
+		echo rtrim($tmpStr. ",")."<br>";
+	}
 }
-echo "<hr>";
-$end = microtime_float() - $start;
-printf("%0.3f seconds\r\n", $end);
+//run the test function
+test();
 ?>
