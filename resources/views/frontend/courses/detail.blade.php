ad988e750de0d9fce5ac79e2c91c264d74a1738e
HoangNH <hoangnhonline@gmail.com>
2017-10-28 19:53:24 +0700

    .

--- resources/views/frontend/courses/detail.blade.php
+++ resources/views/frontend/courses/detail.blade.php
@@ -24,7 +24,7 @@
 						<?php 
 							$coursesArr = [];
 							if( Session::get('userId') ){
-								 $coursesArr = UserCourses::where('user_id', Session::get('userId'))->pluck('courses_id')->toArray();
+								 $coursesArr = DB::table('user_courses')->where('user_id', Session::get('userId'))->pluck('courses_id')->toArray();
 							}
 						?>
 							@if ($detail->score > 0)
