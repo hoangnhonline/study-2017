ecdd19a6e1eefea9bf5d1e7ce73b8b5d1c7be866
HoangNH <hoangnhonline@gmail.com>
2018-09-21 23:14:14 +0700

    .

--- resources/views/backend/subjects/edit.blade.php
+++ resources/views/backend/subjects/edit.blade.php
@@ -45,59 +45,14 @@
               @endif                
                                 
                 
-                <div class="form-group" >
+                 <div class="form-group" >
                   
-                  <label>Họ tên <span class="red-star">*</span></label>
+                  <label>Tên môn học<span class="red-star">*</span></label>
                   <input type="text" class="form-control" name="name" id="name" value="{{ old('name', $detail->name) }}">
-                </div>
-                <span class=""></span>
-                <div class="form-group">                  
-                  <label>Slug <span class="red-star">*</span></label>                  
-                  <input type="text" class="form-control" name="slug"  readonly="readonly" id="slug" value="{{ old('slug', $detail->slug) }}">
-                </div>
-                <div class="form-group" >
-                  
-                  <label>Trường học</label>
-                  <input type="text" class="form-control" name="school_name" id="school_name" value="{{ old('school_name', $detail->school_name) }}">
-                </div>
-                <div class="form-group" style="margin-top:10px;margin-bottom:10px">  
-                  <label class="col-md-3 row">Thumbnail ( 338x190 px)</label>    
-                  <div class="col-md-9">
-                    <img id="thumbnail_image" src="{{ $detail->image_url ? Helper::showImage(old('image_url', $detail->image_url) ) : URL::asset('public/admin/dist/img/img.png') }}" class="img-thumbnail" width="145" height="85">
-                 
-                    <button class="btn btn-default btn-sm btnSingleUpload" data-set="image_url" data-image="thumbnail_image" type="button"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> Upload</button>
-                  </div>
-                  <div style="clear:both"></div>
-                </div>
-                <div style="clear:both"></div>                
-                <!-- textarea -->
-                <div class="form-group">
-                  <label>Mô tả</label>
-                  <textarea class="form-control" rows="6" name="description" id="description">{{ $detail->description }}</textarea>
-                </div> 
-                <div class="form-group">
-                  <div class="checkbox">
-                    <label>
-                      <input type="checkbox" name="is_hot" value="1" {{ $detail->is_hot == 1 ? "checked" : "" }}>
-                      HOT
-                    </label>
-                  </div>               
-                </div>
-                <div class="form-group">
-                  <label>Ẩn/hiện</label>
-                  <select class="form-control" name="status" id="status">                  
-                    <option value="0" {{ $detail->status == 0 ? "selected" : "" }}>Ẩn</option>
-                    <option value="1" {{ $detail->status == 1 ? "selected" : "" }}>Hiện</option>                  
-                  </select>
-                </div>              
-                <div class="form-group">
-                  <label>Chi tiết</label>
-                  <textarea class="form-control" rows="4" name="content" id="content">{{ old('content', $detail->content) }}</textarea>
-                </div>
-                <input type="hidden" id="editor" value="content">
+                </div>      
                   
             </div>          
-            <input type="hidden" name="image_url" id="image_url" value="{{ old('image_url',  $detail->image_url) }}"/>
+           
             <div class="box-footer">
               <button type="submit" class="btn btn-primary btn-sm">Lưu</button>
               <a class="btn btn-default btn-sm" class="btn btn-primary btn-sm" href="{{ route('subjects.index')}}">Hủy</a>
@@ -108,34 +63,7 @@
 
       </div>
       <div class="col-md-4">
-        <!-- general form elements -->
-        <div class="box box-primary">
-          <div class="box-header with-border">
-            <h3 class="box-title">Thông tin SEO</h3>
-          </div>
-        <!-- /.box-header -->
-            <div class="box-body">
-              <input type="hidden" name="meta_id" value="{{ $detail->meta_id }}">
-              <div class="form-group">
-                <label>Meta title </label>
-                <input type="text" class="form-control" name="meta_title" id="meta_title" value="{{ !empty((array)$meta) ? $meta->title : "" }}">
-              </div>
-              <!-- textarea -->
-              <div class="form-group">
-                <label>Meta desciption</label>
-                <textarea class="form-control" rows="6" name="meta_description" id="meta_description">{{ !empty((array)$meta) ? $meta->description : "" }}</textarea>
-              </div>  
-
-              <div class="form-group">
-                <label>Meta keywords</label>
-                <textarea class="form-control" rows="4" name="meta_keywords" id="meta_keywords">{{ !empty((array)$meta) ? $meta->keywords : "" }}</textarea>
-              </div>  
-              <div class="form-group">
-                <label>Custom text</label>
-                <textarea class="form-control" rows="6" name="custom_text" id="custom_text">{{ !empty((array)$meta) ? $meta->custom_text : ""  }}</textarea>
-              </div>
-            
-          </div>    
+           
 
       </div>
       <!--/.col (left) -->      
