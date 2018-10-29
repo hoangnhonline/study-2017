763d92f7c6f5e766e14691d4f7d104b594956723
hoang-nguyen-vn <hoangnhonline@gmail.com>
2018-10-29 14:25:23 +0700

    fix

--- resources/views/frontend/news/child.blade.php
+++ resources/views/frontend/news/child.blade.php
@@ -32,7 +32,7 @@
                         {!! $articleFirst->title !!}
                     </a>
                 </h3>
-                <div class="description">{!! strlen($articleFirst->description) > 160 ? substr($articleFirst->description, 0, 160)."…" : $articleFirst->description !!}</div>
+                <div class="description">{!! strlen($articleFirst->description) > 255 ? substr($articleFirst->description, 0, 255)."…" : $articleFirst->description !!}</div>
             </div><!-- /news-item-first -->
             @endif            
             <div class="news-list clearfix">              
@@ -51,7 +51,7 @@
                       </a>
                   </h3>
                   <p class="text">
-                      {!! strlen($obj->description) > 160 ? substr($obj->description, 0, 160)."…" : $obj->description !!}
+                      {!! strlen($obj->description) > 255 ? substr($obj->description, 0, 255)."…" : $obj->description !!}
                   </p>
                 </div>
               </div><!-- /item -->
